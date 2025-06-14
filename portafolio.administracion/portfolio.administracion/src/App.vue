f46e976b9bc8a0e4563ab8829c9541112917ac5f
Frank ruiz <frank.git96@gmail.com>
2025-06-14 11:52:48 +0200

    administracion: formulario crear proyecto

--- portafolio.administracion/portfolio.administracion/src/App.vue
+++ portafolio.administracion/portfolio.administracion/src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-<main>
+<main class="relative">
   <RouterView />
 </main>
 </template>
--- portafolio.administracion/portfolio.administracion/src/views/panel/ProyectosView.vue
+++ portafolio.administracion/portfolio.administracion/src/views/panel/ProyectosView.vue
@@ -1,8 +1,6 @@
 <template>
-  <ListComponent
-    title="Proyectos"
-    :items="proyectos"
-    :fields="{
+  <button class="btn" @click="form.open()">Nuevo Proyecto</button>
+  <ListComponent title="Proyectos" :items="proyectos" :fields="{
       id: 'id',
       title: 'titulo',
       description: 'descripcion',
@@ -10,10 +8,12 @@
       enableLink: true,
       routeName: 'detalle-proyecto',
       idParam: 'projectId'
-    }"
-    :onEdit="handleEdit"
-    :onDelete="handleDelete"
-  />
+    }" :onEdit="handleEdit" :onDelete="handleDelete" />
+
+
+  <FormularioCrearProyecto ref="form" :usuarioAdministradorId="usuarioId" @created="onProjectCreated">
+  </FormularioCrearProyecto>
+
 </template>
 
 <script setup>
@@ -21,15 +21,24 @@ import ListComponent from '@/components/ListComponent.vue'
 import { useGet } from '@/comporsables/useGet'
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+import FormularioCrearProyecto from '../formularios/FormularioCrearProyecto.vue'
 // Componente para mostrar la lista de proyectos
 const proyectos = ref([])
 const router = useRouter()
+
+const form = ref(null)
+const usuarioId = Number(localStorage.getItem('usuarioId'))
+
 
 let usuarioAdministradorId = localStorage.getItem('usuarioId')
 
 const { data, isLoading, get, isSuccess } = useGet()
 
 onMounted(async () => {
+  await cargarProyectos()
+})
+
+async function cargarProyectos() {
   await get(`/proyecto/usuario/${usuarioAdministradorId}`)
   if (isSuccess.value) {
 
@@ -37,7 +46,7 @@ onMounted(async () => {
   } else {
     console.error('Error al cargar los proyectos')
   }
-})
+}
 
 function handleEdit(item) {
   // Lógica para editar el proyecto
@@ -51,6 +60,15 @@ router.push({
 
 function handleDelete(item) {
   // Lógica para eliminar el proyecto
+
   console.log('Eliminar proyecto:', item)
+}
+
+async function  onProjectCreated(data) {
+  // actualizar lista, notificar, etc.
+  console.log('Proyecto creado:', data)
+await cargarProyectos()
+
+
 }
 </script>
